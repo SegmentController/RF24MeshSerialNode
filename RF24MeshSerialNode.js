796aa3eb88d66102d42b42fa93b26285abdc71b4
BCsabaEngine <csaba@balazs-magyar.hu>
2021-07-10 19:55:32 +0200

    20210710

--- RF24MeshSerialNode.js
+++ RF24MeshSerialNode.js
@@ -4,64 +4,104 @@ const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline');
 
 class RF24MeshSerialNode extends EventEmitter {
-  port = null
-  timeout = 2500
+  static async find(options, foundcb) {
+    for (const port of await SerialPort.list()) {
+      const serialnode = new RF24MeshSerialNode(port.path, options);
 
-  constructor(port, options) {
-    super()
-
-    this.port = port
-    this.timeout = options.timeout || this.timeout
-
-    const parser = this.port.pipe(new Readline({ delimiter: '\r\n' }))
-    parser.on('data', this.onLineData.bind(this))
-
-    if (!port.isOpen)
-      port.open(function (err) {
-        if (err) throw new Error(`Error opening port: ${err.message}`)
-      })
+      serialnode.on('close', () => { console.log("Find Closed:") })
+      serialnode.on('error', (error) => { serialnode.close(); console.log("Find ErrorX:" + error) })
+      serialnode.on('nodevice', () => { serialnode.close(); console.log("Find NO DEVICE") })
+      serialnode.on('ready', () => { foundcb(serialnode); });
+    }
   }
 
+  port = null
+  portnumber = null
+  cmdtimeout = 250
+  isopened = false
+  isready = false
   lastline = ''
-  onLineData(line) {
-    this.emit("read", line);
+  constructor(portnumber, options) {
+    super()
 
-    switch (line) {
-      case "READY":
-        setImmediate(function () {
-          this.emit("ready");
-        }.bind(this))
-        return;
-      case "ERROR Auto renew failed":
-        this.lastline = null
-        return;
-    }
+    this.portnumber = portnumber;
+    this.cmdtimeout = options.cmdtimeout || this.cmdtimeout
+    this.port = new SerialPort(portnumber, {
+      baudRate: 115200,
+    })
 
-    this.lastline = line
+    const inittimer = setTimeout(function () {
+      if (!this.isready)
+        this.emit("nodevice");
+    }.bind(this), options.inittimeout || 2500);
+
+    const parser = this.port.pipe(new Readline({ delimiter: '\r\n' }))
+    parser.on('data', function (line) {
+      this.emit("read", line);
+
+      switch (line) {
+        case "RF24MESHSERIAL READY":
+          setImmediate(function () {
+            clearTimeout(this.inittimer);
+            this.isready = true;
+            this.emit("ready");
+          }.bind(this))
+          return;
+
+        case "ERROR Auto renew failed":
+          this.lastline = null
+          return;
+      }
+
+      this.lastline = line
+    }.bind(this))
+
+    this.port.on("open", function () {
+      this.isopened = true;
+      this.emit("open");
+      console.log("Opened");
+    }.bind(this));
+
+    this.port.on('close', function () {
+      this.emit("close");
+      console.log("Closed");
+    }.bind(this));
+
+    this.port.on("error", function (err) {
+      this.emit("error", err);
+    }.bind(this));
+  }
+
+  close() {
+    if (this.port)
+      if (this.isopened)
+        this.port.close()
   }
 
   async writeLineAndWait(line) {
     this.lastline = null
 
-    this.port.write(`${line}\n`, function (err) { if (err) throw new Error(`Error writing com port: {err.message}`) })
+    this.port.write(`${line}\n`, function (err) {
+      if (err) throw new Error(`Error writing com port: {err.message}`)
+    })
     this.emit("write", line);
 
     const that = this
     return new Promise((resolve, reject) => {
       const start = new Date().getTime()
-      const check = () => {
+      const waiter = () => {
         if (that.lastline && that.lastline.length) {
           if (that.lastline.startsWith("ERROR"))
             return reject(new Error(that.lastline.substring("ERROR".length).trim()))
           return resolve(that.lastline)
         }
-        else if (new Date().getTime() - start > this.timeout) {
+        else if (new Date().getTime() - start > this.cmdtimeout) {
           return reject(new Error("Timeout"))
         }
         else
-          setImmediate(check)
+          setImmediate(waiter)
       }
-      setImmediate(check)
+      waiter()
     })
   }
 
--- examples/simpleNode.js
+++ examples/simpleNode.js
@@ -1,69 +1,128 @@
-const SerialPort = require('serialport')
 const RF24MeshSerialNode = require('../RF24MeshSerialNode.js')
 
-const port = new SerialPort('COM3', {
-  autoOpen: false,
-  baudRate: 115200
-})
+//let node = null;
 
-const node = new RF24MeshSerialNode(port,
-  {
-    timeout: 2000
-  });
+RF24MeshSerialNode.find({
+  inittimeout: 2500,
+  cmdtimeout: 250,
+}, async function (node) {
+  if (node) {
+    console.log("FOUND: " + node.portnumber);
+
+    await node.setNodeId(0)
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.setChannel(90)
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.setSpeed(0)
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getNodeId()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getChannel()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getSpeed()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+    await node.getVersion()
+
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getUptime()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.begin()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+  }
+  else
+    console.log("NOT FOUND");
+});
+
+
+return;
+
+try {
+  node = new RF24MeshSerialNode('COM14',
+    {
+      inittimeout: 2500,
+      cmdtimeout: 250,
+    });
+
+  node.on('error', (error) => {
+    console.log("ErrorX:" + error);
+  })
+
+  node.on('nodevice', () => { console.log("NO DEVICE") })
+
+  node.on('ready', async () => {
+
+    await node.setNodeId(0)
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.setChannel(90)
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.setSpeed(0)
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getNodeId()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getChannel()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getSpeed()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+    await node.getVersion()
+
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    await node.getUptime()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    // try {
+    //   const a = await node.setNodeId(200)
+    //   console.log("OK: " + a)
+    // }
+    // catch (error) { console.log("Error: " + error.message) }
+
+    await node.begin()
+      .then((res) => console.log("OK: " + res))
+      .catch((error) => console.log("ERROR: " + error.message))
+
+    // await node.check()
+    //   .then((res) => console.log("OK: " + res))
+    //   .catch((error) => console.log("ERROR: " + error.message))
+
+    // await node.reset()
+    //   .then((res) => console.log("OK: " + res))
+    //   .catch((error) => console.log("ERROR: " + error.message))
+
+  })
+}
+catch (exception) {
+  console.log("HIBA: " + exception.message);
+}
 
 //node.on('read', (line) => console.log('p->' + line))
 //node.on('write', (line) => console.log('p<-' + line))
-
-node.on('ready', async () => {
-
-  await node.setNodeId(20)
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.setChannel(90)
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.setSpeed(2)
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.getNodeId()
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.getChannel()
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.getSpeed()
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-  await node.getVersion()
-
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.getUptime()
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  // try {
-  //   const a = await node.setNodeId(200)
-  //   console.log("OK: " + a)
-  // }
-  // catch (error) { console.log("Error: " + error.message) }
-
-  await node.begin()
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  await node.check()
-    .then((res) => console.log("OK: " + res))
-    .catch((error) => console.log("ERROR: " + error.message))
-
-  // await node.reset()
-  //   .then((res) => console.log("OK: " + res))
-  //   .catch((error) => console.log("ERROR: " + error.message))
-
-})
